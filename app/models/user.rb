18eacd56fb33e464876aff6d841ee9555618e593
leichaofan <leichaofan2024@gmail.com>
2017-04-20 01:16:16 +0800

    patch avatar of user

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,10 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   before_create :generate_authentication_token
-
+  mount_uploader :avatar, AvatarUploader 
   def generate_authentication_token
     self.authentication_token = Devise.friendly_token
-  end 
+  end
   has_many :reservations
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
